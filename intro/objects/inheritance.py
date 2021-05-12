194a2ed89069022d094966cafcfe225a348777fb
Steven De Avila <stevejd88@gmail.com>
2021-05-12 18:55:27 -0500

    Add Inheritance file

---
+++ intro/objects/inheritance.py
@@ -0,0 +1,32 @@
+class PartyAnimal:
+  x=0
+  name = ""
+
+  def __init__(self, nam):
+    self.name = nam
+    print(self.name, "constructed")
+
+  def party(self) :
+    self.x = self.x + 1
+    print(self.name, "party count", self.x)
+
+
+# putting parantheses after class extends the capabilities of the class inside the parantheses
+class FootballFan(PartyAnimal):
+  points = 0
+  def touchdown(self):
+    self.points = self.points + 7
+    self.party()
+    print(self.name, "points", self.points)
+
+s = PartyAnimal("Sally")
+s.party()
+
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
+
+
+
+
+# INHERITANCE- when we make a new class, and we reuse an existing class to inherit all the capabilities of an existing class. Subclass
